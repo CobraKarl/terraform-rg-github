de7e201a0dfb3d63acd3c0cb8b795e9e39e0f2b0
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-03 14:50:05 +0100

    testing terraform in github

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.91.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = var.subscriptionId
+    client_id = var.clientId
+    client_secret = var.clientSecret
+    tenant_id = var.tenantId
+  features {
+
+  }
+}
+
+resource "azurerm_resource_group" "mkk7" {
+    name = var.RGName
+    location = var.location
+  
+}
